726ffadf8a47b5a7ca7ee697be27330f1d7f7e8a
bumyy32 <bumyy32@gmail.com>
2021-11-29 11:54:14 -0300

    Introdução e testes ao JavaScript
    Variaveis, declaração de variavel, operadores aritmeticos e tipos de variaveis

---
+++ variaveis-operadores/exemplo01.js
@@ -0,0 +1,47 @@
+// Declaração de variaveis
+// let - Variavel LOCAL
+let nome = 'Bummy';
+
+ // mostra a minha variavel nome em um alerta
+alert('Nome:' + nome);
+
+// var - variavel global
+var idade = 19;
+
+alert(idade);
+
+let nome = 'Bummy';
+var idade = 19;
+alert('Nome:' + ' ' + nome + ' ' + '\nIdade: ' + ' ' + idade);
+
+// Fazer a soma de dois números
+
+var numero01, numero02, resultado;
+numero01 = 2254;
+numero02 = 1143;
+
+resultado = numero01 + numero02;
+
+alert('Resultado: ' + resultado);
+alert(numero01 + numero02 + resultado);
+
+// Fazer a subtração
+
+var numero01, numero02, resultado;
+numero01 = 2254;
+numero02 = 1143;
+
+resultado = numero01 - numero02;
+
+alert('Resultado: ' + resultado);
+
+// multiplicação
+
+var numero01, numero02, resultado;
+numero01 = 2254;
+numero02 = 1143;
+
+resultado = numero01 * numero02;
+
+alert('Resultado: ' + resultado);
+
