119c778d78f5b6d7cb8b32840fd1db930094b238
bumyy32 <bumyy32@gmail.com>
2021-12-01 10:32:55 -0300

    Inicio da atividade - Média Escolar
    Nessa atividade, teremos que pegar o nome e as médias dos alunos. Usamos js para fazer toda a ação, bootstrap e css para o visual.

---
+++ Exercicio01 media/index.js
@@ -0,0 +1,8 @@
+var botao = document.querySelector('#calcular') 
+
+botao.addEventListener("click",function(){
+    var nome = document.querySelector('#txtNome').value
+    var nota1 = document.querySelector('txtNota01').value
+    var nota2 = document.querySelector('txtNota02').value
+    
+});
