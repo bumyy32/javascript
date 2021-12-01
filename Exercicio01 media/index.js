8a9026af321934c95a4753faba4abf596a88153e
bumyy32 <bumyy32@gmail.com>
2021-12-01 11:08:55 -0300

    Adicionado campo de resultado
    Somente resultado
    Somente nome
    Nome e resultado

--- Exercicio01 media/index.js
+++ Exercicio01 media/index.js
@@ -1,8 +1,13 @@
 var botao = document.querySelector('#calcular') 
 
 botao.addEventListener("click",function(){
-    var nome = document.querySelector('#txtNome').value
-    var nota1 = document.querySelector('txtNota01').value
-    var nota2 = document.querySelector('txtNota02').value
-    
+    var nome = document.querySelector('#txtNome').value;
+    var nota1 = parseInt(document.querySelector('#txtNota01').value);
+    var nota2 = parseInt(document.querySelector('#txtNota02').value);
+
+    var media = (nota1 + nota2) / 2;
+
+    document.querySelector('#result-nome').innerHTML = nome;
+    document.querySelector('#resultado').innerHTML = media;
+    document.querySelector('#resultadomedia').innerHTML = "Nome do aluno: " + nome + " Média geral: " + media;
 });
