ed44e1b72c2310ab08050a5856275a3246b53788
bumyy32 <bumyy32@gmail.com>
2021-12-02 09:19:23 -0300

    Códigos e visual
    Para o visual, foi usado bootstrap e css
    Para os códigos, foi usado js

---
+++ variaveis-operadores/funções/js/calculadora.js
@@ -0,0 +1,18 @@
+function calc(numero1,numero2, operador){
+     return eval(`${numero1} ${operador} ${numero2}`);
+}
+
+
+
+// var botao = document.getElementById('btn-calcular')
+
+// botao.addEventListener("click", function(){
+// var numero1 = parseInt(document.getElementById("numero1").value);
+// var numero2 = parseInt(document.getElementById("numero2").value);
+// var operador = document.getElementById("operador").value;
+
+// var resultado = numero1 + operador + numero2;
+
+// document.getElementById("resultado").innerHTML = 'Resultado:' + resultado
+
+// });
