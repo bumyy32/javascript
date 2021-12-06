99085242e3f65ee449db91b014b55dab1052fdc6
bumyy32 <bumyy32@gmail.com>
2021-12-06 10:40:02 -0300

    Exercicio ordem crescente
    inicio do exercicio de ordem crescente numerica usando js

---
+++ variaveis-operadores/exercicios/exercicio-crescente/js/index.js
@@ -0,0 +1,9 @@
+var botao = document.querySelector('#btn-calcular');
+
+botao.addEventListener('click', function media1(numero1, numero2, numero3) {
+    var numero1 = parseInt(document.querySelector('#numero1').value);
+    var numero2 = parseInt(document.querySelector('#numero2').value);
+    var numero3 = parseInt(document.querySelector('#numero3').value);
+
+    
+})
