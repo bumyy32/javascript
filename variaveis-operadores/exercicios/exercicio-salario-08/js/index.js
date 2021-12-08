ffe8175a7629b3e9a4018355ce5336f8e6e9d3ca
bumyy32 <bumyy32@gmail.com>
2021-12-08 08:59:51 -0300

    Adição css e js
    adicionado comandos css
    fazendo js

--- variaveis-operadores/exercicios/exercicio-salario-08/js/index.js
+++ variaveis-operadores/exercicios/exercicio-salario-08/js/index.js
@@ -1,32 +1,22 @@
-document.querySelector('#btn-calcular').addEventListener("click", function(){
-    let nome = document.querySelector('#txtNome').value;
-    let valorH = document.querySelector('#txtValor').value;
-    let tHoras = document.querySelector('#txtTrabalhadas').value;
-    let menu = document.querySelector('#txtMenu').value;
-    var vTrasporte = 0;
-    var iRenda = 0;
-    var sBruto = valorH * tHoras;
-    var pagamento = ('O salario bruto de ' + nome + ' é ' + sBruto);
+var botao = document.querySelector("btn-calcular")
 
-    if (sBruto > 1440.00){
-        iRenda = sBruto * 0.11;
-    }
-    else{
-        alert('Não Possui Imposto De Renda')
-    }
+botao.addEventListener('click', function(){
+    var nome = document.querySelector("#txtNome").value;
+    var valorhora = parseFloat(document.querySelector('#txtvalorhora').value);
+    var quanthoras = parseFloat(document.querySelector('#txtquanthoras').value);
+    var valetransporte = parseFloat(document.querySelector('txtvaletransporte').value)
 
-    switch(menu) {
-        case 'sim':
-            vTrasporte = sBruto * 0,06;
-            var pagamento = ('O valor de desconto de vale transporte é ' + vTrasporte);
-        break;
+    var salariobruto = valorhora * quanthoras;
+    var valorVt = 0;
+    var valorIR = 0;
+
+    switch(valetransporte) {
+        case "sim":
+            valorVt = salariobruto * 0.06;
+            break;
     }
 
-    var sLiquido = sBruto - vTrasporte - iRenda;
-    var pagamento = ('O Salario liquido é ' + sLiquido)
-
-    document.querySelector('#resultado').innerHTML = `<div class="alert alert-success" role="alert">
-    <h4 class="alert-heading">resultado</h4>
-    <p>Resultado: ${pagamento}</p>
-</div>`;
-});
+    if (salarioBruto > 1400) {
+        valorIR = salariobruto * 0.11;
+    }
+})
