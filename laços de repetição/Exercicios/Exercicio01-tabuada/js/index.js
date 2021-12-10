858bbce074a9c5727f3926a7897753266d0414ad
bumyy32 <bumyy32@gmail.com>
2021-12-10 09:31:38 -0300

    apagar após novo resultado

--- laços de repetição/Exercicios/Exercicio01-tabuada/js/index.js
+++ laços de repetição/Exercicios/Exercicio01-tabuada/js/index.js
@@ -2,6 +2,7 @@ document.querySelector('#btn-calcular').addEventListener('click',
 function() {
     var numero = parseInt(document.querySelector('#txtTabuada').value);
     var tabuada = numero * 1;
+    document.querySelector('#tabuada').innerHTML = "";
     for (var i = 1; i <= 10; i++){
         tabuada = numero * i;
         var result = document.createElement('p');
