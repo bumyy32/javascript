90aaa8c8e079e18d637e9d008cd10c6ad359dd99
bumyy32 <bumyy32@gmail.com>
2021-12-10 09:09:42 -0300

    laço completo

--- laços de repetição/Exercicios/Exercicio01-tabuada/js/index.js
+++ laços de repetição/Exercicios/Exercicio01-tabuada/js/index.js
@@ -4,5 +4,8 @@ function() {
     var tabuada = numero * 1;
     for (var i = 1; i <= 10; i++){
         tabuada = numero * i;
+        var result = document.createElement('p');
+        result.innerHTML = tabuada;
+        document.querySelector('#tabuada').appendChild(result);
     }
 })
